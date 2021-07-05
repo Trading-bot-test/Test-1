b98df789f8121005a3efd05586bfec6fd08f1c18
Hemanthbysani <hemanthbysani2002@gmail.com>
2021-07-05 19:32:53 +0530

    Login and signup push
    pushing first batch

---
+++ login.js
@@ -0,0 +1,27 @@
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if (username === "admin" && password === "admin") {
+        alert("You have successfully logged in.");
+        alert("Redirecting to Dashboard");
+        window.location = "work_in_progress.jpg"
+    }
+    else if (username == null || username == "") {
+        alert("Please enter the username.");
+        return false;
+    }
+    else if (password == null || password == "") {
+        alert("Please enter the password.");
+        return false;
+    }
+    else {
+        alert("Wrong password entered.");
+        loginErrorMsg.style.opacity = 1;
+    }
+})
