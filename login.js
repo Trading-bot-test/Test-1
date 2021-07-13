baf9fe0c3336c7e9c5a97748a805a03a27028293
Hemanthbysani <hemanthbysani2002@gmail.com>
2021-07-13 22:53:46 +0530

    login animations added

--- login.js
+++ login.js
@@ -2,6 +2,7 @@ const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
 
+
 loginButton.addEventListener("click", (e) => {
     e.preventDefault();
     const username = loginForm.username.value;
@@ -25,4 +26,3 @@ loginButton.addEventListener("click", (e) => {
         loginErrorMsg.style.opacity = 1;
     }
 })
-{/* <script defer src="login.js"></script> */}
