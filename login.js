52721db044afef67da00256a0b38cbeebbc3da6a
Hemanthbysani <hemanthbysani2002@gmail.com>
2021-07-18 18:07:49 +0530

    small changes

--- login.js
+++ login.js
@@ -1,17 +1,15 @@
 const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
-const loginErrorMsg = document.getElementById("login-error-msg");
 
 
 loginButton.addEventListener("click", (e) => {
-    e.preventDefault();
     const username = loginForm.username.value;
     const password = loginForm.password.value;
 
     if (username === "admin" && password === "admin") {
         alert("You have successfully logged in.");
         alert("Redirecting to Dashboard");
-        window.location = "./Dashboard.html"
+        window.location.href = "./Dashboard.html";
     }
     else if (username == null || username == "") {
         alert("Please enter the username.");
@@ -23,7 +21,8 @@ loginButton.addEventListener("click", (e) => {
     }
     else {
         alert("Wrong password entered.");
-        loginErrorMsg.style.opacity = 1;
+        window.location.href = "./login.html";
+        return false;
     }
 })
 
